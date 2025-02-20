d55a84d223018c26da95372255c454a8117334ad
Nik <46762556+EnKdev@users.noreply.github.com>
2025-02-20 17:14:13 +0100

    Update publish-linux.sh
    Bugfixing the unix scripts rn

--- BuildScripts/Unix/publish-linux.sh
+++ BuildScripts/Unix/publish-linux.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Creates a self contained build in /bin/Release/net8.0/linux-x64/publish/
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
 dotnet publish ../../Archipelago.RetroArchClient.csproj --configuration Release --self-contained -r linux-x64
