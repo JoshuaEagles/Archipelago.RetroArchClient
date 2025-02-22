5e8b6a8aec5e520ab08d7e264409f200382de9ec
EnKdev <sesoui72@googlemail.com>
2025-02-22 22:37:15 +0100

    Translate all location entries from AllLocationInformation.cs and split them up into their own respective YAML files.
    Also removed a few unnecessary comments at this point.

--- Archipelago.RetroArchClient/OcarinaOfTime/Models/LocationInformation.cs
+++ Archipelago.RetroArchClient/OcarinaOfTime/Models/LocationInformation.cs
@@ -2,10 +2,6 @@ using Archipelago.RetroArchClient.OcarinaOfTime.Enums;
 
 namespace Archipelago.RetroArchClient.OcarinaOfTime.Models;
 
-// Maybe add documentation detailing what this model class is for, what it represents, etc.
-// Helps to get other developers interested in helping with the client up to speed.
-
-// See Enums.GameModes for example on how this could be achieved.
 public class LocationInformation(string name, LocationType type, byte offset, byte bitToCheck, Area area)
 {
 	public string Name { get; } = name;
