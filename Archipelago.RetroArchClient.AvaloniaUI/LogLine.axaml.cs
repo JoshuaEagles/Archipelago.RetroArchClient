ba536bf62fcbeb24fd7f6a4907d0e014e97f42b8
JoshuaEagles <joshuaeagles1@gmail.com>
2025-02-23 00:56:27 -0500

    clean up MainWindow and move LogLine into a component to better show the plan

---
+++ Archipelago.RetroArchClient.AvaloniaUI/LogLine.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Archipelago.RetroArchClient.AvaloniaUI;
+
+public partial class LogLine : UserControl
+{
+	public LogLine()
+	{
+		InitializeComponent();
+	}
+}
