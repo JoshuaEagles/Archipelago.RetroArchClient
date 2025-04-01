e327438f4245ce265b7cbfed98af5a53331c7526
araganor <araganor@gmail.com>
2025-04-01 14:25:21 -0500

    Set up unit test project
    - added basic unit test for config file loading

---
+++ Archipelago.RetroArchClient.Tests/Utils/FileUtilsTests.cs
@@ -0,0 +1,27 @@
+﻿using Archipelago.RetroArchClient.Configuration;
+using Archipelago.RetroArchClient.Utils;
+
+namespace Archipelago.RetroArchClient.Tests.Utils;
+
+public class FileUtilsTests
+{
+    private const string TestFileFolderName = "TestFiles";
+
+    [Theory]
+    [InlineData("config.valid.1.json")]
+    public void TryLoadJsonFile_LoadsConfigurationFile(string fileName)
+    {
+        var expected = new ConfigurationSettings(
+                new ArchipelagoServer("archipelago.gg", 38281, "", ""),
+                new RetroArch("localhost", 55355));
+
+        var path = Path.Combine(Environment.CurrentDirectory, TestFileFolderName, fileName);
+        var success = FileUtils.TryLoadJsonFile(path, out ConfigurationSettings result);
+
+
+        Assert.True(success);
+        Assert.Equal(expected, result);
+
+    }
+
+}
